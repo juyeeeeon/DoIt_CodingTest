8c58043e03e86a92fd1ecece25fba101234700d9
juyeeeeon <dlwndus35@yonsei.ac.kr>
2022-12-31 00:30:25 +0900

    22/12/30

---
+++ codingTest/src/ex03_1_배열과리스트/P1546_평균.java
@@ -0,0 +1,24 @@
+package ex03_1_배열과리스트;
+
+import java.util.Scanner;
+
+public class P1546_평균 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		double sum = 0;
+		double max = 0;
+		
+		for(int i=0; i<N; i++) {
+			int temp = sc.nextInt();
+			sum += temp;
+			if(temp>max) max = temp;
+		}
+		
+		System.out.println(sum/max*100/N);
+	}
+
+}
