e6eca4f6cd59854d019e459a571fedd236f2abbf
juyeeeeon <dlwndus35@yonsei.ac.kr>
2022-12-31 00:41:58 +0900

    22/12/30

--- codingTest/src/ex03_2_구간합구하기4/P11659_구간합구하기4.java
+++ codingTest/src/ex03_2_구간합구하기4/P11659_구간합구하기4.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class P11659_ {
+public class P11659_구간합구하기4 {
 	//합 배열 공식 : S[i] = S[i-1] + A[i]
 	//구간 합 공식 : S[j] - S[i-1]
 	public static void main(String[] args) throws IOException {
