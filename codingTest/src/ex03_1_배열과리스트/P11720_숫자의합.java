69bfbe8b30ddae650879fd05a55d710884fde742
juyeeeeon <dlwndus35@yonsei.ac.kr>
2022-12-31 00:22:15 +0900

    22/12/30

---
+++ codingTest/src/ex03_1_배열과리스트/P11720_숫자의합.java
@@ -0,0 +1,23 @@
+package ex03_1_배열과리스트;
+
+import java.util.Scanner;
+
+public class P11720_숫자의합 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String sNum = sc.next();
+		
+		char[] cNum = sNum.toCharArray(); //문자열의 각 문자를 배열로
+		int sum = 0;
+		
+		for(int i=0; i<N; i++) {
+			sum += cNum[i] - '0'; //문자를 숫자로
+		}
+		
+		System.out.println(sum);
+	}
+
+}
